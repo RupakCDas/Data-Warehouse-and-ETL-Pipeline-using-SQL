c1d53ee420e45827327e0f2a2652b25b32230898
Rupak Chandra Das <rupakcdp48@gmail.com>
2026-06-28 21:11:28 +0600

    Update database_creating.sql

--- scripts/database_creating.sql
+++ scripts/database_creating.sql
@@ -2,7 +2,7 @@
 Create Database and Schemas
 ========================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    This script creates a new database named 'data_warehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 */
